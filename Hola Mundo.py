2754da63c3fa2ba2e490c73e6447b669943f9840
LeKilleRGallet <arico@unal.edu.co>
2020-10-17 16:26:33 -0500

    se agrego un metodo a la funcion

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -2,7 +2,5 @@
 
 def HolaMundo(nombre, emocion = "sin emociones"):
     return f"hola {nombre} estoy {emocion}"
-    
-
 hola_mundo = HolaMundo("mundo", "feliz")
-print(hola_mundo)
+print(hola_mundo.upper())
