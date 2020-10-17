d5017c04f19b4ed22b5b63e8c05a982d675a9d6c
LeKilleRGallet <arico@unal.edu.co>
2020-10-17 16:10:50 -0500

    se agrego un return

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -1,7 +1,8 @@
 
 
 def HolaMundo(nombre, emocion = "sin emociones"):
-    hola_mundo = f"hola {nombre} estoy {emocion}"
-    print(hola_mundo)
+    return f"hola {nombre} estoy {emocion}"
+    
 
-HolaMundo("mundo")
+hola_mundo = HolaMundo("mundo", "feliz")
+print(hola_mundo)
