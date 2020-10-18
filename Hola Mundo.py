9b80062fdfaac62045dc310390bb820d102b951f
LeKilleRGallet <arico@unal.edu.co>
2020-10-18 05:31:42 -0500

    se agrego una suma

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -1,6 +1,7 @@
 
 
-def HolaMundo(nombre, emocion = "sin emociones"):
-    return f"hola {nombre} estoy {emocion}"
-hola_mundo = HolaMundo("mundo", "feliz")
+def HolaMundo(nombre, emocion = "sin emocion", a = 0, b = 0):
+    return f"hola {nombre} estoy {emocion} porque se sumar {a} + {b} que es {a + b}"
+
+hola_mundo = HolaMundo("mundo", "feliz", 2, 3)
 print(hola_mundo.upper())
