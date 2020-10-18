c2e5e87c36da6dbbdc8411113261028219d7c5b1
LeKilleRGallet <arico@unal.edu.co>
2020-10-18 08:47:19 -0500

    se agregaron metodos a la lista

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -1,5 +1,7 @@
 def HolaMundo(nombre, emocion = "sin emocion", V1 = 0, V2 = 0):
-    return f"hola {nombre} estoy {emocion} porque se que la operacion {valor[1]} + {valor[2]} es igual a {valor[1] + valor[2]}"
+    return f"hola {nombre} estoy {emocion} porque se que la operacion {V1} + {V2} es igual a {V1 + V2}"
 valor = [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9]
-hola_mundo = HolaMundo("mundo", "feliz", valor[1], valor[2])
+valor.pop(2) #elimina valor con la primera posicion de la lista, dandole ahora a el Valor 1 la primera posicion
+valor.remove(1) #Busca el Valor "1" dentro de la lista y lo elimina de ella
+hola_mundo = HolaMundo("mundo", "feliz", valor[0], valor[2])
 print(hola_mundo.upper())
