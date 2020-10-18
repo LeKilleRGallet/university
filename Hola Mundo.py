92c9f559230be65090920c5384221069626fab39
LeKilleRGallet <arico@unal.edu.co>
2020-10-18 08:35:32 -0500

    se agrego una lista de valores

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -1,7 +1,5 @@
-
-
-def HolaMundo(nombre, emocion = "sin emocion", a = 0, b = 0):
-    return f"hola {nombre} estoy {emocion} porque se sumar {a} + {b} que es {a + b}"
-
-hola_mundo = HolaMundo("mundo", "feliz", 2, 3)
+def HolaMundo(nombre, emocion = "sin emocion", V1 = 0, V2 = 0):
+    return f"hola {nombre} estoy {emocion} porque se que la operacion {valor[1]} + {valor[2]} es igual a {valor[1] + valor[2]}"
+valor = [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9]
+hola_mundo = HolaMundo("mundo", "feliz", valor[1], valor[2])
 print(hola_mundo.upper())
