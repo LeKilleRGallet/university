260c9e04504ad557f3eaff0ad9845a797aa996fe
LeKilleRGallet <arico@unal.edu.co>
2020-10-19 17:53:33 -0500

    se agrego un if anidado

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -6,8 +6,12 @@ valor.pop(2) #elimina valor con la tercera posicion de la lista, dandole ahora a
 print(valor)
 valor.remove(0) #Busca el Valor "0" dentro de la lista y lo elimina de ella
 print(valor)
-if valor[0] == 1: #si el valor con la primera posicion es "1" entonces se eliminara el valor con la tercera posicion de la lista
-    valor.pop(2)
+if valor[0] == 1: #si el valor con la primera posicion es "1" entonces se eliminara el valor con la cuarta posicion de la lista
+    valor.pop(3)
+print(valor)
+if not 2 in valor: #si no se encuentra el valor "2" en la lista, se devuelve un true
+    if 1 in valor: #si se encuentra el valor "1" en la lista se devuelve un true
+        valor.remove(3) #se elimina el valor "3" de la lista
 print(valor)
 hola_mundo = HolaMundo("mundo", "feliz", valor[0], valor[2])
 print(hola_mundo.upper())
