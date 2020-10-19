bbfa997f07800318d8aea2bb753f02ee1c4d4c7e
LeKilleRGallet <arico@unal.edu.co>
2020-10-19 16:17:45 -0500

    se agrego el condicional if

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -1,7 +1,13 @@
 def HolaMundo(nombre, emocion = "sin emocion", V1 = 0, V2 = 0):
     return f"hola {nombre} estoy {emocion} porque se que la operacion {V1}+{V2} es igual a {V1+V2}"
 valor = [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9]
-valor.pop(2) #elimina valor con la segunda posicion de la lista, dandole ahora a el Valor 3 la segunda posicion
-valor.remove(1) #Busca el Valor "1" dentro de la lista y lo elimina de ella
+print(valor)
+valor.pop(2) #elimina valor con la tercera posicion de la lista, dandole ahora a el Valor 3 la tercera posicion
+print(valor)
+valor.remove(0) #Busca el Valor "0" dentro de la lista y lo elimina de ella
+print(valor)
+if valor[0] == 1: #si el valor con la primera posicion es "1" entonces se eliminara el valor con la tercera posicion de la lista
+    valor.pop(2)
+print(valor)
 hola_mundo = HolaMundo("mundo", "feliz", valor[0], valor[2])
 print(hola_mundo.upper())
