1c8655e4783c8a3b77f8e17f7cd4db949773f8c4
LeKilleRGallet <arico@unal.edu.co>
2021-01-15 14:24:58 -0500

    se redondeo el valor del dolar a 2 cifras

--- Convertor de divisas.py
+++ Convertor de divisas.py
@@ -1,7 +1,8 @@
-COP = input('que cantidad de pesos quieres convertir?')
+COP = input('que cantidad de pesos quieres convertir?: ')
 COP = float(COP)
 USDCOP = 3400
 USD = COP / USDCOP
+USD = round(USD, 2)
 USD = str(USD)
 COP = str(COP)
 print (COP + ' pesos es igual a ' + USD + ' dolares')
