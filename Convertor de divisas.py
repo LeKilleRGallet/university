29501cb87e500386f676da3fb50fc87e28a32e66
LeKilleRGallet <arico@unal.edu.co>
2021-01-15 14:22:19 -0500

    se creo una conversion de divisas de pesos a dolares

---
+++ Convertor de divisas.py
@@ -0,0 +1,7 @@
+COP = input('que cantidad de pesos quieres convertir?')
+COP = float(COP)
+USDCOP = 3400
+USD = COP / USDCOP
+USD = str(USD)
+COP = str(COP)
+print (COP + ' pesos es igual a ' + USD + ' dolares')
