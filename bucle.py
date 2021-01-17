754825e31a77e433674ea2629cc71ea4efb8eca2
LeKilleRGallet <arico@unal.edu.co>
2021-01-17 12:42:29 -0500

    se mejoro sintaxis

--- bucle.py
+++ bucle.py
@@ -1,11 +1,11 @@
 def main():
   LIMITE = 1000000
-  potencia = 0
   contador = 0
+  potencia = 2**contador
   while potencia < LIMITE:
-    potencia = 2**contador
-    print (f"2 elevado a {contador} es igual a {potencia}")
+    print (f"2 elevado a {contador} es igual a: {potencia}")
     contador = contador + 1
+    potencia = 2**contador
 
 if __name__ == "__main__":
   main()
