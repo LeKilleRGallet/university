d15d3b5748e6a57a99c9c5ecb8ceefa9d2de69de
LeKilleRGallet <arico@unal.edu.co>
2022-01-07 23:24:51 -0500

    optimized algorithm

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,55 @@
 import time
+from datetime import datetime
+import os
 
-def prime(n):
-    #validate if a number is prime
+def clear():
+    if os.name == "posix":
+        os.system ("clear")
+    elif os.name == ("ce", "nt", "dos"):
+        os.system ("cls")
+
+def prime_validator(n, primes):
     if n<=1:
         return True
-    elif n==2:
+    elif n in (2,3,5,7):
         return False
     else:
-        for i in range(2,int(n**0.5)+1):
-            # print(i)
-            if n%i==0:
+        sqrt_n = n**0.5
+        if n == int(sqrt_n)**2:
+            return True
+        else:
+            for i in primes:
+                if i > (int(sqrt_n)+1):
+                    return False
+                if n%i == 0:
+                    return True
+
+def prime(element, primes):
+    while True:
+        try:
+            if element not in (2,3,5,7) and 0 in {element%2, element%3, element%5, element%7}:
                 return True
+            else:
+                if primes[-1]>(element**0.5):
+                    if prime_validator(element, primes) != True:
+                        return False
+                    else:
+                        return True
+                else:
+                    for i in range(primes[-1]+1, round(element**0.5)+2):
+                        if prime_validator(i, primes) != True:
+                            primes.append(i)
+                    if primes[-1]<(element**0.5):
+                        for i in range(primes[-1]+1, primes[-1]**2):
+                            if prime_validator(i, primes) != True:
+                                primes.append(i)
+                                break
+        except IndexError:
+            if element <= 1:
+                return True
+            elif element == 2:
+                primes.append(element)
+                return False
 
 class FiboIter():
 
@@ -54,6 +93,8 @@ class FiboIter():
                 raise StopIteration
 
 def run():
+    clear()
+    primes=[]
     while True:
         try:
             range_min = input('insert floor: ')
@@ -93,11 +134,28 @@ def run():
             fprime = input('Invalid input, just y or n, please try again: ')
 
     fibonacci = FiboIter(min=range_min, max=range_max)
+
+    c = 1
+    lrange_max = 0
+
+    if fprime and range_min != None:
+        for i in range((range_min**0.5)+1):
+            if prime_validator(i, primes) != True:
+                primes.append(i)
+
+    if fprime and range_max != None:
+        lrange_max = len(str(range_max))
+    elif fprime and range_max == None:
+        lrange_max = 17
+
+    time_0 = datetime.now()
     for element in fibonacci:
         if element != None:
             if fprime:
-                if prime(element) != True:
-                    print(f'{element: < {len(str(range_max))}} is a fibonacci prime [index: {fibonacci.counter}]')
+                if prime(element, primes) != True:
+                    print(f'{str(c)+")":>3}{element: > {lrange_max}} is a fibonacci prime [index: {fibonacci.counter:>3}] Execution time: {str((datetime.now() - time_0).total_seconds()):>8} seconds')
+                    c += 1
+                    time_0 = datetime.now()
             else:
                     print(element)
         time.sleep(0.05)
